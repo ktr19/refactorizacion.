384e24c71d4051e252aa7926ed9e0793f1466163
KevinV <kavillarreala@gmail.com>
2024-02-29 17:04:23 +0100

    Iniciar repositorio. Crear Codigo Comun.

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -13,7 +13,7 @@ public class Calculadora {
 
     public int resultado;
     
-    public int sumar(int a, int b) {
+    public  int sumar(int a, int b) {
         return a + b;
     }
 
@@ -39,17 +39,20 @@ public class Calculadora {
         return a % b;
     }
     
-    public static int operacion(int a, int b, String operacion) {
-        if ("sumar".equals(operacion)) {
-            return a + b;
-        }else if ("restar".equals(operacion)) {
-            return a - b;
-        }else if ("multiplicar".equals(operacion)) {
-            return a * b;
-        }else if ("dividir".equals(operacion)) {
-            return a / b;
-        }else if ("modulo".equals(operacion)) {
-            return a % b;
+    public  int operacion(int a, int b, String operacion) {
+        if (null != operacion) switch (operacion) {
+            case "sumar":
+                return sumar(a, b);
+            case "restar":
+                return restar(a ,b);
+            case "multiplicar":
+                return multiplicar(a, b);
+            case "dividir":
+                return dividir(a, b);
+            case "modulo":
+                return modulo(a, b);
+            default:
+                break;
         }
         
         throw new IllegalArgumentException("Operación no válida");
