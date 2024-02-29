20c3a55813b4b20711b63ec157eb0c2dc3e2d68c
KevinV <kavillarreala@gmail.com>
2024-02-29 17:12:42 +0100

    Refactorizar borrado seguro.

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package com.ud4refactorizar.utilidades;
+package calculadora;
 
 /**
  *
@@ -59,7 +59,4 @@ public class Calculadora {
     }
   
 
-  public static int operacionObsoleta(int a, int b) {
-        return a + b;
-  }
 }
--- src/calculadora/Multiplicar.java
+++ src/calculadora/Multiplicar.java
@@ -2,16 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.ud4refactorizar.utilidades;
+package calculadora;
 
 /**
  *
  * @author usuario
  */
-public class Sumar {
+public class Multiplicar {
     
-    public static int sumar(int a, int b) {
-        return a + b;
+    public static int multiplicar(int a, int b) {
+        return a * b;
     }
     
 }
